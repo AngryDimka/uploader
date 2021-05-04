bf7af9327accec45f77af5e120d4969c5f0bfc67
Dimka <bazhenov.dimasik@gmail.com>
2021-05-04 20:53:19 +0300

    create

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let inputs = document.querySelectorAll('.input__file');
+        Array.prototype.forEach.call(inputs, function (input) {
+            let label = input.nextElementSibling,
+                labelVal = label.querySelector('.input__file-button-text').innerText;
+        
+            input.addEventListener('change', function (e) {
+                let countFiles = '';
+                if (this.files && this.files.length >= 1) {
+                    countFiles = this.files.length;
+                }
+                if (countFiles) {
+                    label.querySelector('.input__file-button-text').innerText = 'Загружено файлов: ' + countFiles;
+                } else {
+                    label.querySelector('.input__file-button-text').innerText = labelVal;
+                }
+            });
+        });
+});
